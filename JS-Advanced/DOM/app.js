80f59a716e56c9736fd4bc127fbfa4960009e618
Verzhinia Parvanova <76648218+VerzhiniaP@users.noreply.github.com>
2021-02-10 13:15:16 +0200

    Add files via upload

--- JS-Advanced/DOM/app.js
+++ JS-Advanced/DOM/app.js
@@ -1,56 +1,16 @@
-function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
-   const input = document.querySelector('#inputs>textArea')
-   const bestRestaurantP = document.querySelector('#bestRestaurant>p')
-   const workersP = document.querySelector('#workers>p')
-   function onClick () {
-     let arr = JSON.parse(input.value)
-     let obj = {}
-        for(let line of arr){
-           let tokens = line.split(' - ')
-           let name = tokens[0]
-           let workersArr = tokens[1].split(', ')
-           let workers = []
-           let averageSalary = 0
-
-           for(let worker of workersArr){
-              let workerTokens = worker.split(' ')
-              const salary = Number(workerTokens[1])
-              workers.push({
-                 name: workerTokens[0], salary
-              })
-              }
-
-              if(obj[name]){
-               workers = workers.concat(obj[name].workers) 
-              }
-              workers.sort((a,b) => b.salary - a.salary)
-              let bestSalary = workers[0].salary
-              averageSalary = workers.reduce((sum,worker)=> sum + worker.salary,0)/workers.length
-
-              obj[name]={
-                 workers,
-                 averageSalary,
-                 bestSalary
-              }
- }
-let bestReastaurantSalary = 0
-let bestReastaurant = undefined
-for(let name in obj){
- if(obj[name].averageSalary > bestReastaurantSalary){
-     bestReastaurant = {name, 
-     workers: obj[name].workers,
-     bestSalary: obj[name].bestSalary,
-     averageSalary: obj[name].averageSalary
-}
-bestReastaurantSalary = obj[name].averageSalary
- }
-}
-bestRestaurantP.textContent = `Name: ${bestReastaurant.name} Average Salary: ${bestReastaurant.averageSalary.toFixed(2)} Best Salary: ${bestReastaurant.bestSalary.toFixed(2)}`
-let result = []
-bestReastaurant.workers.forEach(worker =>
-   result.push(`Name: ${worker.name} With Salary: ${worker.salary}`))
-
-workersP.textContent = result.join(" ")
-   }
-}
+function addItem() {
+    let ul = document.querySelector('#items')
+    let input = document.querySelector('#newItemText').value
+    let li = document.createElement('li')
+    li.textContent = input
+    let btn = document.createElement('a')
+    btn.textContent = '[Delete]'
+    btn.href="#"
+    btn.addEventListener('click', (ev)=> {
+        ev.target.parentNode.remove()
+    })
+    
+    li.appendChild(btn)
+    ul.appendChild(li)
+    input.value = ''
+    }
